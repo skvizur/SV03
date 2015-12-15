e175b59ab43649bd3fff0cd031f69bb2a5c84fa6
hildurrun <hildurg14@ru.is>
2015-12-15 08:55:34 +0000

    -2 skrar

--- world.py
+++
@@ -1,5 +0,0 @@
-import vincent
-world_countries = r'world-countries.json'
-world = vincent.Map(width=1200, height=1000)
-world.geo_data(projection='winkel3', scale=200, world=world_countries)
-world.to_json(path)
